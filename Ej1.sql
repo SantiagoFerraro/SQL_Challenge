3649c75b802bc4f0a8614319ecabefc17b24dc44
Santiago Ferraro <santiferraro16@gmail.com>
2022-04-12 14:28:16 -0300

    Añadir drop table ej 1

--- Ej1.sql
+++ Ej1.sql
@@ -7,6 +7,7 @@ CREATE TABLE Ej1(
 INSERT INTO Ej1 (Value)
 VALUES (-56),(76),(-84),(96),(-47);
 
-UPDATE Ej1 SET Value = Value * -1  #Multiplicamos todos los valores por -1 lo cual hara que los positivos cambien a negativos y los negativos a positivos
+UPDATE Ej1 SET Value = Value * -1;  #Multiplicamos todos los valores por -1 lo cual hara que los positivos cambien a negativos y los negativos a positivos
 
+DROP TABLE IF EXISTS Ej1
 
