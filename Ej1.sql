e9e703c50adf1c2062289a5bb71da90dcbcca8f0
Santiago Ferraro <santiferraro16@gmail.com>
2022-04-12 14:15:35 -0300

    Create Ej1.sql

---
+++ Ej1.sql
@@ -0,0 +1,12 @@
+USE SQLChallenge;
+
+CREATE TABLE Ej1(
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Value INT(11) NOT NULL); 
+
+INSERT INTO Ej1 (Value)
+VALUES (-56),(76),(-84),(96),(-47);
+
+UPDATE Ej1 SET Value = Value * -1  #Multiplicamos todos los valores por -1 lo cual hara que los positivos cambien a negativos y los negativos a positivos
+
+
