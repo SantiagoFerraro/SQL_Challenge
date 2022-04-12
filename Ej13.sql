040d228cb286fd763f2cb00d15c6f847d2347c71
Santiago Ferraro <santiferraro16@gmail.com>
2022-04-12 17:52:04 -0300

    Create Ej13.sql

---
+++ Ej13.sql
@@ -0,0 +1,20 @@
+SELECT * FROM sqlchallenge.ej13;
+
+USE SQLChallenge;
+
+CREATE TABLE Ej13 (
+id int(11) NOT NULL auto_increment primary key,
+Name char(60) NOT NULL,
+Age int(11) NOT NULL
+);
+
+INSERT INTO Ej13 (Name,Age)
+VALUES ('Bob',21),('Sam',19),('Jill',18),('Jim',21),('Sally',19),('Jess',20),('Will',21);
+
+DELIMITER 
+CREATE PROCEDURE Edades(in Edad int)
+BEGIN
+    select count(Age) FROM Ej13 WHERE Age=Edad ;
+END;
+
+CALL Edades(20)
